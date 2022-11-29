416da6d245dec0b71cc0e64a1176085f5780376b
mikias <mikiasabiy16@gmail.com>
2022-11-29 18:10:58 -0500

    add: 11-pow.py: function to calculate power numbers

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# pow - calculates a the power of b
+# @a: The number to be powered.
+# @b: The number that powers @a.
+# Return: The powered result.
+def pow(a, b):
+    return a ** b
