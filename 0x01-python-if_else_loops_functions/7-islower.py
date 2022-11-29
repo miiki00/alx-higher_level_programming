67b81c84e3b37b0cfd2ba2a106c3bb34c8391deb
mikias <mikiasabiy16@gmail.com>
2022-11-29 15:58:52 -0500

    add: 7-islower.py: function that check if a letter is upper case or not

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+# islower - function that checks if a char is lower case or not
+# @c: The character to check.
+# Return: True if lower case, False if not.
 def islower(c):
     n = ord(c)
     if n >= ord('a') and n <= ord('z'):
