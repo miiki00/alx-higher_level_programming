d37a420e60593911e1ab1c92f91f3b78919fd87e
mikias <mikiasabiy16@gmail.com>
2022-12-01 16:55:43 -0500

    update: 2-args.py: user massage changed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,9 @@ import sys
 
 if __name__ == "__main__":
     length = len(sys.argv)
-    print("{} arguments{}".format(length - 1, '.' if length == 1 else ':'))
+    if length == 1:
+        print("0 arguments.")
+    else:
+        print("{} argument{}".format(length - 1, ':' if length == 2 else 's:'))
     for i in range(1, length):
         print("{}: {}".format(i, sys.argv[i]))
