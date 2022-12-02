bcccc420ea0d9f963bb4d0a7dfeb08e3c4cefa80
mikias <mikiasabiy16@gmail.com>
2022-12-02 13:46:03 -0500

    update: 100-my_calculator.py: user messages changed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,20 +4,19 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("{}".format(len(sys.argv)))
         print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
         exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     match (sys.argv[2]):
         case '+':
-            print('{} + {} = {}'.format(a, b, add(a, b)))
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
         case '-':
-            print('{} - {} = {}'.format(a, b, sub(a, b)))
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
         case '*':
-            print('{} * {} = {}'.format(a, b, mul(a, b)))
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
         case '/':
-            print('{} / {} = {}'.format(a, b, div(a, b)))
+            print('{:d} / {:d} = {:.0f}'.format(a, b, div(a, b)))
         case _:
-            print('Unknown operator. Avalable operators: +, -, * and /')
+            print('Unknown operator. Available operators: +, -, * and /')
             exit(1)
