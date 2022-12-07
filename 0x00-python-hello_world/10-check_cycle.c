04ee5db9feceaeffd8f99eddcb67128354fcacce
mikias <mikiasabiy16@gmail.com>
2022-12-06 20:32:27 -0500

    update: 10-check_cycle.c: unecessary fucntions removed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,73 +1,6 @@
 #include "lists.h"
 
 /**
- * array_len - counts the length of a listint pointers array.
- * @arr: The array to count the length of.
- *
- * Return: The length of the array (success), -1 (error)
- */
-ssize_t array_len(listint_t **arr)
-{
-	ssize_t len;
-
-	if (arr == NULL)
-		return (-1);
-	for (len = 0; *arr != NULL; len++)
-		arr++;
-	return (len);
-}
-
-/**
- * array_malloc - allocats @size number of memory for listint_t pointers
- * and initializes their value to null and also adds 1 more for the
- * terminating NULL value of the array.
- * @size: The number  of memory to allocate
- *
- * Return: The address of the array (success), NULL (error)
- */
-listint_t **array_malloc(size_t size)
-{
-	listint_t **memo = NULL;
-	size_t i;
-
-	if (size == 0)
-		return (NULL);
-	size = size + 1;
-	memo = malloc(sizeof(listint_t *) * size);
-	if (memo == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		memo[i] = NULL;
-	return (memo);
-}
-
-/**
- * array_realloc - reallocats memory for a listint_t array by adding @size
- * size to the current value. and also it copies the old value to the newly
- * allocated place.
- * @arr_ptr: The address of the array.
- * @size: The size to add to the current size.
- *
- * Return: The address of the new array (success), NULL (error).
- */
-listint_t **array_realloc(listint_t **arr_ptr, size_t size)
-{
-	listint_t **new = NULL;
-	ssize_t tmp;
-
-	if (arr_ptr == NULL || size == 0)
-		return (arr_ptr);
-	tmp = array_len(arr_ptr) + size; /* the size of the new memory */
-	new = array_malloc(tmp);
-	if (new == NULL)
-		return (NULL);
-	for (tmp = 0; arr_ptr[tmp] != NULL; tmp++)
-		new[tmp] = arr_ptr[tmp];
-	free(arr_ptr);
-	return (new);
-}
-
-/**
  * check_cycle - checks if there is a cycle in a listint_t list.
  * @list: The head of the list to check cycle in.
  *
@@ -75,28 +8,18 @@ listint_t **array_realloc(listint_t **arr_ptr, size_t size)
  */
 int check_cycle(listint_t *list)
 {
-	listint_t **dump = NULL;
+	listint_t *dump = NULL, *copy = NULL;
 	ssize_t i, j;
 
 	if (list == NULL || list->next == NULL)
 		return (0);
-	dump = array_malloc(50);
-	if (dump == NULL)
-		return (0);
-	for (i = 0; list != NULL; list = list->next, i++)
+	for (i = 0, copy = list; copy != NULL; copy = copy->next, i++)
 	{
-		for (j = 0; dump[j] != NULL; j++)
+		for (dump = list, j = 0; j != i; dump = dump->next, j++)
 		{
-			if (dump[j] == list)
-			{
-				free(dump);
+			if (dump == copy)
 				return (1);
-			}
 		}
-		if (j == 50 * (i / 50) && i != 0)
-			dump = array_realloc(dump, 50);
-		dump[i] = list;
 	}
-	free(dump);
 	return (0);
 }
