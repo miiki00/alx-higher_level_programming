4e510ba930d6be6dbe652bb8c149673afb5aba85
mikias <mikiasabiy16@gmail.com>
2022-12-20 01:37:39 -0500

    update: 100-safe_print_integer_err.py: newline feed included when printing to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,8 @@ from sys import stderr
 def safe_print_integer_err(value):
     try:
         "{:d}".format(value)
-    except Exception as err:
-        stderr.write("Exception: {}".format(err))
+    except BaseException as err:
+        stderr.write("Exception: {}\n".format(err))
         return (False)
     else:
         print("{:d}".format(value))
