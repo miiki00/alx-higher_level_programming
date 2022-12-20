3a7144bb9611a68236fe76cfce22dbf1e1d2d0e0
mikias <mikiasabiy16@gmail.com>
2022-12-20 00:02:16 -0500

    update: 2-safe_print_list_integers.py: hot fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     printed, j = 0, 0
-    
-    if type(my_list) is not list or type(x) is not int:
-        raise TypeError
-
-    while printed < x:
+    while j < x:
         try:
             print("{:d}".format(my_list[j]), end='')
         except IndexError:
