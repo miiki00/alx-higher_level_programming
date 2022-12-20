b433340089cbd21712b071c3fbac46a639538279
mikias <mikiasabiy16@gmail.com>
2022-12-20 02:02:50 -0500

    update: 101-safe_function.py: hot fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ from sys import stderr
 def safe_function(fct, *args):
     try:
         return_value = fct(args[0], args[1])
-    except BaseException as err:
+    except Exception as err:
         stderr.write("Exception: {}\n".format(err))
         return (None)
     else:
