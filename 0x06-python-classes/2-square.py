e9058bf2a33b362856d26acaa5de2a353f02929a
mikias <mikiasabiy16@gmail.com>
2022-12-21 01:24:25 -0500

    add: 2-square.py: contains a class that defines a square object with a specific size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+""" 0-square
+Contains a class that defines a square
+"""
+
+
+class Square:
+
+    """ Square - A class that defines a square object.
+
+    This is a description for the __init__ method
+    Args:
+        size (:obj: 'int', options): The size of the square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
