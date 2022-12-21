881f5c50653f41cd6213c2bd58e408e9c18cb353
mikias <mikiasabiy16@gmail.com>
2022-12-21 01:02:43 -0500

    add: 1-square.py: contains a class that defines a square object with a specific size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+""" 0-square
+Contains a class that defines a square
+"""
+
+
+class Square:
+
+    """ Square - A class that defines a square object.
+
+    This is a description for the __init__ method
+    Args:
+        size (int): The size of the square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        self.__size = size
