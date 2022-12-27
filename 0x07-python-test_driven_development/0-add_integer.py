5c146b915a59d63ed4557161c27a2da8e76ad197
mikias <mikiasabiy16@gmail.com>
2022-12-26 21:31:35 -0500

    add: 0-add_integer.py: a module containing a function that adds to integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains a function that adds to number.
+Functions:
+    add_integer: add to integers.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    This function adds two number a and b and the
+    returns the result.
+
+    Args:
+        a (int): The first number to add.
+        b (int): The second number to add. if not supplied
+                 defaults to 98.
+
+    Return:
+        The sum of the two numbers.
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return (int(a + b))
