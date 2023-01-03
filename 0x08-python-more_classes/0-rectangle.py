7537bc4c5d8c7cd6bf467eb512f20607d00e5cb3
mikias <mikiasabiy16@gmail.com>
2023-01-02 23:20:26 -0500

    add: 0-rectangle.py: a defination of a class for Rectangle objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 0-rectangel : module
+contains a class defination and more related to rectangles.
+
+Class:
+    Rectangle: used to define a Rectangle object.
+"""
+
+
+class Rectangle:
+    pass
