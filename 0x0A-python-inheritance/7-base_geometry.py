959d1e5ddcc43a3f334da2e6c925a748ad6728cd
mikias <mikiasabiy16@gmail.com>
2023-01-11 06:24:08 -0500

    update: 7-base_geometry.py: a little coditional statment evalueation mistake fixed

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -40,5 +40,5 @@ class BaseGeometry:
         """
         if (type(value) is not int):
             raise TypeError("{} must be an integer".format(name))
-        elif (value < 0):
+        elif (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))
