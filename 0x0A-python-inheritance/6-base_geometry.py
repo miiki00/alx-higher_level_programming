f5e56a0138e36e6d3ca9a8dd4e6055c4da0ec03d
mikias <mikiasabiy16@gmail.com>
2023-01-11 05:34:16 -0500

    add: 6-base_geometry.py: method that calculates aread adde

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains the defination of the class BaseGeometry.
+
+Class:
+    BaseGeometry:
+
+Functions:
+    No functions.
+"""
+
+
+class BaseGeometry:
+    """ BaseGeometry: Class
+    A BaseGeometry class defination.
+    """
+
+    def area(self):
+        """ area : method
+        This method calculates the area of a BaseGeometry object
+
+        Args:
+            No arguments.
+
+        Return:
+            The area of the BaseGeometry object.
+        """
+        raise Exception("area() is not implemented")
