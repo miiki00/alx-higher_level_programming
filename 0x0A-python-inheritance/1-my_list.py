dfe79f73f7d231d3917fd709498948ccb1b1abde
mikias <mikiasabiy16@gmail.com>
2023-01-12 07:13:32 -0500

    update: 1-mk_list.py: word 'from' removed from the documetation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,43 +1,15 @@
 #!/usr/bin/python3
 """
-This module contains a class that is inherited list and
-provides a method that  print a list in a sorted manner with
-out changing the orginal list.
-
-Classes:
-    MyList : This is a class that inherited list and
-    provides a method that  print a list in a sorted manner with
-    out changing the orginal list.
-
-Functions:
-    No functions.
+contains definations of MyList.
 """
 
 
 class MyList(list):
-    """ MyList : Class.
-    This a class that inherite the class list and
-    provides a method that  print a list in a sorted manner with
-    out changing the orginal list.
+    """ Inherited from list"""
 
-    It inherits list's constructor class so you can look more
-    informationn of the on the documentation for the list class.
-    """
     def print_sorted(self):
-        """ print_sorted : method
-        This method prints a MyList instance that contains a list of
-        int objects in a sorted manner.
-
-        Args:
-            No Arguments.
-
-        Return:
-            You can uncomment the return statment if you need for any
-            reasons I have used to test the function.
-        """
-        copyList = self.copy()
-        copyList.sort()
-
+        """ prints a list sorted"""
+        copyList = sorted(self)
         print(copyList)
         # if you want you can uncommnet the return statment.
         # return (copyList)
