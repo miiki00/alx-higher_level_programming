959737b537d4de6cc66e328fa79b721ca2443824
mikias <mikiasabiy16@gmail.com>
2023-01-12 07:20:41 -0500

    update: 1-my_list.py: reverted back to the last version

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,38 @@
 #!/usr/bin/python3
 """
-contains the MyList class
+This module contains a class that is inherited list and
+provides a method that  print a list in a sorted manner with
+out changing the orginal list.
+Classes:
+    MyList : This is a class that inherited list and
+    provides a method that  print a list in a sorted manner with
+    out changing the orginal list.
+Functions:
+    No functions.
 """
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
-
+    """ MyList : Class.
+    This a class that inherite the class list and
+    provides a method that  print a list in a sorted manner with
+    out changing the orginal list.
+    It inherits list's constructor class so you can look more
+    informationn of the on the documentation for the list class.
+    """
     def print_sorted(self):
-        """prints the sorted list"""
-        print(sorted(self))
+        """ print_sorted : method
+        This method prints a MyList instance that contains a list of
+        int objects in a sorted manner.
+        Args:
+            No Arguments.
+        Return:
+            You can uncomment the return statment if you need for any
+            reasons I have used to test the function.
+        """
+        copyList = self.copy()
+        copyList.sort()
+
+        print(copyList)
+        # if you want you can uncommnet the return statment.
+        # return (copyList)
