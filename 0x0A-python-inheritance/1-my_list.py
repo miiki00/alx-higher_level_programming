60e361e066af1237c598f5c4ec16f457df08778d
mikias <mikiasabiy16@gmail.com>
2023-01-12 07:15:06 -0500

    update: 1-my_list.py: whole implementaion changed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-contains definations of MyList.
+contains the MyList class
 """
 
 
 class MyList(list):
-    """ Inherited from list"""
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
-        """ prints a list sorted"""
-        copyList = sorted(self)
-        print(copyList)
-        # if you want you can uncommnet the return statment.
-        # return (copyList)
+        """prints the sorted list"""
+        print(sorted(self))
