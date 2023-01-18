0e2f70eb9a84358314c49f0a914edce06fe8de73
mikias <mikiasabiy16@gmail.com>
2023-01-18 14:47:58 -0500

    add: 0-read_file.py: contains a function that prints a content of file to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This Module contains a functiont that reads a file
+and print the content to the standard out put.
+
+Functions:
+    read_file(filename=""):
+        Reads a file named filename and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """ read_file: function
+    This function read a file and prints it's content
+    to the standard output.
+
+    Args:
+        filename (str): The name of the file to read.
+
+    Return:
+        None.
+    """
+    if not filename:
+        return
+    with open(filename) as myFile:
+        print(myFile.read(), end="")
