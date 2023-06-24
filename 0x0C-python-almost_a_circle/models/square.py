6d0a2b3a924b71e3c1c1685fe6a41616715249ce
miiki00 <mikiasabiy16@gmail.com>
2023-06-24 13:04:56 -0400

    update: square.py: __str__ method overloaded

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,17 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        __str__: returns representation of the square object.
+
+        Args:
+            None.
+
+        Return:
+            String representation of the square object.
+        """
+        st_format = f"[{type(self).__name__}] ({self.id}) {self.x}/{self.y}\
+ - {self.width}/{self.height}"
+        return (st_format)
