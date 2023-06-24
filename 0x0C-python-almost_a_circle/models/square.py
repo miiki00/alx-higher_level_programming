3d839af9f6a05c04dad56a2b75f423557f553408
miiki00 <mikiasabiy16@gmail.com>
2023-06-24 13:24:25 -0400

    update: square.py: update method added

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,6 +30,37 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    def update(self, *args, **kwargs):
+        """
+        update: updates attributes of instance.
+
+        Args:
+            1st: id
+            2nd: size
+            3rd: x
+            4th: y
+          or keyworded:
+            id (int): Unique id to identify the object.
+            size (int): The height and width to update the rectangle
+                        object with.
+            x (int): The postion of the rectangle in x axis.
+            y (int): The postion of the rectangle in y asix.
+
+        Return:
+            None.
+        """
+        if len(args) == 0:
+            for key, value in kwargs.items():
+                self.__setattr__(key, value)
+        else:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
+
     def __str__(self):
         """
         __str__: returns representation of the square object.
@@ -43,3 +74,33 @@ class Square(Rectangle):
         st_format = f"[{type(self).__name__}] ({self.id}) {self.x}/{self.y}\
  - {self.width}"
         return (st_format)
+
+    @property
+    def size(self):
+        """size : property.
+        getter property for the private instance attributes __width and
+        __height.
+
+        Args:
+            No arguments.
+
+        Return:
+            The width==height of the self rectangle object.
+        """
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        """size: property.
+        setter property for the privated instance attributes __width
+        and __height.
+
+        Args:
+            value (int): The value to set to the private instance attribute.
+                         __widht and __height.
+
+        Reuturn:
+            None.
+        """
+        self.width = value
+        self.height = value
