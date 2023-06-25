7f80d27562c421319eab679d65e29f56f47c4e25
miiki00 <mikiasabiy16@gmail.com>
2023-06-25 02:01:25 -0400

    update: base.py: new static method from_json_string added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,9 +37,20 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        """
         if list_dictionaries is not None:
             return (json.dumps(list_dictionaries))
         return ("[]")
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        
+        """
+        if json_string is not None and len(json_string) !=  0:
+            return (json.loads(json_string))
+        return ([])
 
     @classmethod
     def save_to_file(cls, list_objs):
