901f62164f398d2b8d9d50cc4a0d1f51519d7417
Mikias <mikiasabiy16@gmail.com>
2023-07-06 08:41:37 -0400

    add: 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- creats a user called user_0d_1 on localhost.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
