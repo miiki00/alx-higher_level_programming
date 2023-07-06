4e512fa2722742d04d4d2db70e75eb41852f605d
Mikias <mikiasabiy16@gmail.com>
2023-07-06 11:44:04 -0400

    add: 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@ SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
 FROM `tv_show_genres`
 RIGHT JOIN `tv_shows`
 ON `tv_show_genres`.`show_id` = `tv_show`.`id`
-WHERE `tv_show_genres`.`show_id` IS NULL
+WHERE `tv_show_genres`.`genre_id` IS NULL
 ORDER BY `tv_shows.title`;
