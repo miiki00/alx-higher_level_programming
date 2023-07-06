7d01e50f58984655a63363ea5b20ebe9c7327dee
Mikias <mikiasabiy16@gmail.com>
2023-07-06 11:25:50 -0400

    add: 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- lists all shows contained in the database hbtn_0d_tvshows.
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-RIGHT JOIN tv_shows
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+FROM `tv_show_genres`
+RIGHT JOIN `tv_shows`
+ON `tv_show_genres`.`show_id` = `tv_show`.`id`
+ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
