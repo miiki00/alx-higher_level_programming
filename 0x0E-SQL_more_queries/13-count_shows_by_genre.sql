bfe1a5d0b035f4e5576412921adaaace91e9ff50
Mikias <mikiasabiy16@gmail.com>
2023-07-07 05:38:38 -0400

    add: 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- counts shows by genres.
-SELECT `tv_genres`.`name` AS `genres`, COUNT(`tv_show_genres`.`genre_id`) AS `number_of_shows`
+SELECT `tv_genres`.`name` AS `genre`, COUNT(`tv_show_genres`.`genre_id`) AS `number_of_shows`
 FROM `tv_genres`
 RIGHT JOIN `tv_show_genres`
 ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
-GROUP BY `tv_genres`.`name`
+GROUP BY `genre`
 ORDER BY `number_of_shows` DESC;
