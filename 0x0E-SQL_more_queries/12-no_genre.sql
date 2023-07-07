b7f47d17119f5d57cf53d0d367ea885666a03dac
root <root@localhost.localdomain>
2023-07-07 05:08:27 -0400

    add: 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,6 @@
 SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
 FROM `tv_show_genres`
 RIGHT JOIN `tv_shows`
-ON `tv_show_genres`.`show_id` = `tv_show`.`id`
+ON `tv_show_genres`.`show_id` = `tv_shows`.`id`
 WHERE `tv_show_genres`.`genre_id` IS NULL
-ORDER BY `tv_shows.title`;
+ORDER BY `tv_shows`.`title`;
