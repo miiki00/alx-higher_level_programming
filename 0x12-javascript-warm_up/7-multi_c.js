7ebb949e6e8a37ad4678ffd785938cd0045a8f70
Mikias <mikiasabiy16@gmail.com>
2023-07-23 11:45:58 -0400

    add: 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const numFirstArg = Number(firstArg);
+if (typeof(firstArg) === typeof(undefined)) {
+  console.log('Missing number of occurrences');
+} else if (numFirstArg) {
+  for (let i = 0; i < numFirstArg; i++) {
+    console.log('C is fun');
+  }
+}
