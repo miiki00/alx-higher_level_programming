92eb74c732e75c69f9d9b49c7dddc02e35c13cf4
Mikias <mikiasabiy16@gmail.com>
2023-07-28 00:31:32 -0400

    add: 0-rectangle.js: A class that defines a rectangle object

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,13 @@
+#!/home/qelem/Documents/node-v14.4.0-linux-x64/bin/node
+
+/**
+ * Rectangle: A class that defines a rectangle object.
+ *
+ * This is a documentation for the constructor function.
+ * Args:
+ *  None.
+ */
+class Rectangle {
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,6 @@
+# JavaScript Objects Scopes Closures
+
+## Description
+
+This repo contains scripts that are wirtten in order to understand about objects, scopes, closures in Javascript.
+Feel free to look around.
